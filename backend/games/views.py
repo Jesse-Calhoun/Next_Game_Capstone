b67b8ddb7cf59e8faf438ea2a4236e54e86738f6
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-03-06 18:24:57 -0500

    I built out the start of the urls file to go with the get_all_games views function, which I built out in the views file, I added the api/games/ and the games.urls file to the urls file of the main project.

--- backend/games/views.py
+++ backend/games/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Game
+from .serializers import GameSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_games(request):
+    if request.method == 'GET':
+        games = Game.objects.filter()
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
