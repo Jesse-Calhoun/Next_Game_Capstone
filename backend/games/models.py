e3e52c7e726b9df10d22bffcedc2d98233f34825
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-03-06 16:30:02 -0500

    Built out the model for the games app.

--- backend/games/models.py
+++ backend/games/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+class Game(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    game_type = models.CharField(max_length=200)
+    players_at_game = models.IntegerField()
+    next = models.BooleanField()
+    indoor = models.BooleanField()
+    attendees = models.IntegerField()
