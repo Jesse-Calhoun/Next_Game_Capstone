2e0c943dc044cabeddffd12bb42fc2b3557f8fd7
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-03-07 14:11:24 -0500

    Created the Put functionailty in views file for games, using the function named edit_games + created a path for the put functionality and the edit_games function in the urls.py file of the games app.

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns  = [
     path('', views.get_all_games),
     path('create_game/', views.create_game),
-    path('<int:pk>/', views.game_details)
+    path('<int:pk>/',views.game_details),
+    path('<int:pk>/edit/', views.edit_game)
 ]
