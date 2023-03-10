4d2cea399def5aedfc0ad3cb1f681da5f9d40bb8
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-03-10 17:46:28 -0500

    Created the folders and files for the component I initially think I will be needing for this project, most currently are empty.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,21 +12,21 @@ import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 
 // Util Imports
-import PrivateRoute from "./utils/PrivateRoute";
+// import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route
+        {/* <Route
           path="/"
           element={
             <PrivateRoute>
-              <HomePage />
             </PrivateRoute>
           }
-        />
+        /> */}
+        <Route path='' element={<HomePage />}/>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
